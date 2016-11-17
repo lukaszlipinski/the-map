1747ccc3a7c4b9d59a74fc1555f84107aba0b274
Łukasz Lipiński <llipinski@getresponse.com>
2016-11-17 15:12:25 +0100

    last version

--- js/map.js
+++ js/map.js
@@ -47,10 +47,29 @@
     };
 
     window.map.draw = function(canvas) {
-        this.addSingleParkingSpot(canvas, 50, 50);
+        var offset = 100;
+        var space = 20;
+        var spotWidth = this.spotWidth;
+
+        var data = {
+            spots : [
+                {posX: offset, posY: 50, count: 2, direction: 'top'},
+                {posX: offset + spotWidth * 2 + space, posY: 50, count: 3, direction: 'top'},
+                {posX: offset + spotWidth * 5 + space * 2, posY: 50, count: 3, direction: 'top'},
+                {posX: offset + spotWidth * 8 + space * 3, posY: 50, count: 3, direction: 'top'},
+                {posX: offset + spotWidth * 11 + space * 4, posY: 50, count: 1, direction: 'top'},
+                {posX: offset + spotWidth * 12 + space * 5, posY: 50, count: 2, direction: 'top'}
+            ]
+        };
+
+        for(var i = 0; i < data.spots.length; i++) {
+            var spot = data.spots[i];
+
+            this.addParkingSpot(canvas, spot);
+        }
     };
 
-    window.map._drawFirstSegment = function(canvas, posX, posY) {
+    window.map._drawFirstSegment = function(canvas, posX, posY, numberOfSpots) {
         var line1 = new fabric.Line([0, 0, 0, this.spotHeight], $.extend({
             left: posX,
             top: posY
@@ -71,9 +90,9 @@
         canvas.add(line2);
     };
 
-    window.map._drawLastSegment = function(canvas, posX, posY) {
+    window.map._drawLastSegment = function(canvas, posX, posY, numberOfSpots) {
         var line3 = new fabric.Line([0, 0, 0, this.spotHeight], $.extend({
-            left: posX + this.spotWidth,
+            left: posX + this.spotWidth * numberOfSpots,
             top: posY
         }, {
             stroke: this.strokeColor,
@@ -83,7 +102,7 @@
         canvas.add(line3);
 
         var line5 = new fabric.Line([0, 0, this.strokeWidth * 1.5, 0], $.extend({
-            left: posX + this.spotWidth - this.strokeWidth / 2,
+            left: posX + this.spotWidth * numberOfSpots - this.strokeWidth / 2,
             top: posY + this.spotHeight
         }, {
             stroke: this.strokeColor,
@@ -93,8 +112,8 @@
         canvas.add(line5);
     };
 
-    window.map._drawTopLine = function(canvas, posX, posY) {
-        var line = new fabric.Line([0, 0, this.spotWidth, 0], $.extend({
+    window.map._drawTopLine = function(canvas, posX, posY, numberOfSpots) {
+        var line = new fabric.Line([0, 0, this.spotWidth * numberOfSpots, 0], $.extend({
             left: posX,
             top: posY
         }, {
@@ -105,10 +124,41 @@
         canvas.add(line);
     };
 
-    window.map.addSingleParkingSpot = function(canvas, posX, posY) {
-        this._drawTopLine(canvas, posX, posY);
-        this._drawFirstSegment(canvas, posX, posY);
-        this._drawLastSegment(canvas, posX, posY);
+    window.map._drawMiddleLine = function(canvas, posX, posY, sportNumber) {
+        var line3 = new fabric.Line([0, 0, 0, this.spotHeight], $.extend({
+            left: posX + this.spotWidth * sportNumber,
+            top: posY
+        }, {
+            stroke: this.strokeColor,
+            strokeWidth: this.strokeWidth
+        }));
+
+        canvas.add(line3);
+
+        var line5 = new fabric.Line([0, 0, this.strokeWidth * 2, 0], $.extend({
+            left: posX + this.spotWidth * sportNumber - this.strokeWidth / 2,
+            top: posY + this.spotHeight
+        }, {
+            stroke: this.strokeColor,
+            strokeWidth: this.strokeWidth
+        }));
+
+        canvas.add(line5);
+    };
+
+    window.map.addParkingSpot = function(canvas, spot) {
+        var posX = spot.posX;
+        var posY = spot.posY;
+        var numberOfSpots = spot.count;
+
+        this._drawTopLine(canvas, posX, posY, numberOfSpots);
+        this._drawFirstSegment(canvas, posX, posY, numberOfSpots);
+
+        for (var i = 1; i <= numberOfSpots - 1; i++) {
+            this._drawMiddleLine(canvas, posX, posY, i);
+        }
+
+        this._drawLastSegment(canvas, posX, posY, numberOfSpots);
     };
 
 }());
